bd9733f7fd92a240dff6bb32bf2bd4499d298908
kubersharma001 <kubersharma001@gmail.com>
2021-04-24 06:22:49 +0000

    lisa: Refactor libinit and handle more variants
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Change-Id: I86d9d0488a204c2ed8648cfae9a27b2040c9645c

--- libinit/init_lisa.cpp
+++ libinit/init_lisa.cpp
@@ -27,7 +27,9 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <cstdlib>
 #include <fstream>
+#include <string.h>
 #include <unistd.h>
 #include <vector>
 
@@ -39,24 +41,82 @@
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using std::string;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+};
 
 void property_override(char const prop[], char const value[], bool add = true) {
     prop_info *pi;
 
     pi = (prop_info *)__system_property_find(prop);
-    if (pi) {
+    if (pi)
         __system_property_update(pi, value, strlen(value));
-    } else if (add) {
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void set_device_props(const std::string fingerprint, const std::string description,
+        const std::string brand, const std::string device, const std::string model) {
+    const auto set_ro_build_prop = [](const std::string &source,
+                                      const std::string &prop,
+                                      const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+                                        const std::string &prop,
+                                        const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    for (const auto &source : ro_props_default_source_order) {
+        set_ro_build_prop(source, "fingerprint", fingerprint);
+        set_ro_product_prop(source, "brand", brand);
+        set_ro_product_prop(source, "device", device);
+        set_ro_product_prop(source, "model", model);
     }
+
+    property_override("ro.build.fingerprint", fingerprint.c_str());
+    property_override("ro.build.description", description.c_str());
+    property_override("ro.bootimage.build.fingerprint", fingerprint.c_str());
+    property_override("ro.system_ext.build.fingerprint", fingerprint.c_str());
 }
 
 void vendor_load_properties() {
+//   SafetyNet workaround
+    char const fp[] = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
+    char const fp_desc[] = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
 
-    // Safetynet Workaround
+    string region = android::base::GetProperty("ro.boot.hwc", "");
+
+    if (region == "IN") {
+        set_device_props(
+            fp,
+            fp_desc,
+            "Xiaomi", "lisa", "2109119DI");
+    } else if (region == "GLOBAL") {
+        set_device_props(
+            fp,
+            fp_desc,
+            "Xiaomi", "lisa", "2109119DG");
+    } else {
+        set_device_props(
+            fp,
+            fp_desc,
+            "Xiaomi", "lisa", "2109119DG");
+    }
+
+//  SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
     property_override("ro.oem_unlock_supported", "0");
-    property_override("ro.build.description", "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys");
-    property_override("ro.build.fingerprint", "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys");
-    property_override("ro.system.build.fingerprint", "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys");
 }
+
