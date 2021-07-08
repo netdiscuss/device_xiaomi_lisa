684f3db88211d9d9ed6000c250a37067444e4730
Michael Bestas <mikeioannina@gmail.com>
2021-07-08 15:33:47 +0200

    lisa: overlay: Add call recording overlay
    Change-Id: I6bb12aad39d42b76927ea4c97ec640c1f001ec3c

--- device.mk
+++ device.mk
@@ -61,7 +61,8 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayLisa \
-    WifiResOverlayLisa
+    WifiResOverlayLisa \
+    DialerOverlayLisa
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
