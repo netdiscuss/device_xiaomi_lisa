50eb49d949c2919aacf4d552a0877082fbfd58ea
Lineu Martins <lineumartins001@gmail.com>
2021-07-08 23:07:25 +0200

    lisa: overlay: Set FP Location
    Change-Id: I9e8323bd407b863a2b25b357020136e3ed1f0934

--- device.mk
+++ device.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayLisa \
+    SettingsOverlayLisa \
     SystemUIOverlayLisa \
     WifiResOverlayLisa \
     DialerOverlayLisa
