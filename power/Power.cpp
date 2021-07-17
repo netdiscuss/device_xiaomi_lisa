c66b88a9b4489097701fc244028f676a68ce04af
Arian <arian.kulmer@web.de>
2021-07-18 01:54:53 +0200

    lisa: power: implement double tap to wake
    Change-Id: I73f95e32daf9e2c702662ac2c2c8182e6b8eb214

--- power/Power.cpp
+++ power/Power.cpp
@@ -35,9 +35,20 @@
 
 #include <aidl/android/hardware/power/BnPower.h>
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
+
+#include <sys/ioctl.h>
+
+#define SET_CUR_VALUE 0
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+
+#define TOUCH_MAGIC 0x5400
+#define TOUCH_IOC_SETMODE TOUCH_MAGIC + SET_CUR_VALUE
+
+#define Touch_Doubletap_Mode 14
 
 using ::aidl::android::hardware::power::BnPower;
 using ::aidl::android::hardware::power::IPower;
@@ -60,7 +71,12 @@ void setInteractive(bool interactive) {
 ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(INFO) << "Power setMode: " << static_cast<int32_t>(type) << " to: " << enabled;
     switch(type){
-        case Mode::DOUBLE_TAP_TO_WAKE:
+        case Mode::DOUBLE_TAP_TO_WAKE: {
+            int fd = open(TOUCH_DEV_PATH, O_RDWR);
+            int arg[2] = {Touch_Doubletap_Mode, enabled ? 1 : 0};
+            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
+            break;
+        }
         case Mode::LOW_POWER:
         case Mode::LAUNCH:
         case Mode::EXPENSIVE_RENDERING:
