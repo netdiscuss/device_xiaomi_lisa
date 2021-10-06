a50351dab7f34ceeb81df95e6c5b7dd39eb7a6dc
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-06 18:58:40 +0530

    lisa: props: Set priv-app permissions to log

--- properties/default.mk
+++ properties/default.mk
@@ -4,5 +4,8 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Get rid of that by overriding it in /product on eng builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
-    ro.adb.secure=0
 endif
+
+# Priv-app permission
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.control_privapp_permissions=log
