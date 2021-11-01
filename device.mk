d8f5df836e0f3dccb821639ad1d90fb4b3483ddb
ganeshi4u <ganeshvarmai4u@gmail.com>
2021-11-01 19:05:32 +0000

    lisa: overlays: Give RRO's its own folder

--- device.mk
+++ device.mk
@@ -101,6 +101,10 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# RRO Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayLisa \
     FrameworkResOverlayLisa \
