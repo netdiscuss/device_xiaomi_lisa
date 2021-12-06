65e4f7576e96be2045a7f406abe6fe5dbe6e724c
johnmart19 <johnivan19999@gmail.com>
2021-12-06 07:06:36 +0200

    lisa: props: Switch to skiagl threaded

--- properties/default.mk
+++ properties/default.mk
@@ -1,3 +1,9 @@
 # Priv-app permission
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.control_privapp_permissions=log
+
+# Render Engine
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.hwui.renderer=skiagl \
+	debug.renderengine.backend=skiaglthreaded \
+	renderthread.skia.reduceopstasksplitting=true
