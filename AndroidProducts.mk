231c11376109f161f4fa7c8c0e04690e54e0a59a
Karan Parashar <whyredfire@gmail.com>
2022-01-06 19:47:44 +0530

    lisa: init PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2021 PixelOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lisa.mk
+    $(LOCAL_DIR)/aosp_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lisa-eng \
-    arrow_lisa-userdebug \
-    arrow_lisa-user
+    aosp_lisa-eng \
+    aosp_lisa-userdebug \
+    aosp_lisa-user
--- aosp_lisa.mk
+++ aosp_lisa.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2021 PixelOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common PixelOS configurations
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -20,13 +21,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Xiaomi 11 Lite NE
-PRODUCT_NAME := arrow_lisa
-
-# Build info
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="lisa" \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+PRODUCT_NAME := aosp_lisa
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
