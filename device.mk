e82b9916b0fe02fee5c22dac2cbdebe4d0914f65
Karan Parashar <whyredfire@gmail.com>
2022-01-06 22:14:21 +0530

    lisa: Drop ifaa boot jars

--- device.mk
+++ device.mk
@@ -106,9 +106,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Kernel
 PRODUCT_COPY_FILES += \
     $(LISA_PREBUILT)/kernel/dtb.img:dtb.img
