2c1eee5407657d08bca42e2f5d4272aae7c31eda
Karan Parashar <whyredfire@gmail.com>
2022-01-22 20:30:53 +0530

    lisa: Drop adb props

--- properties/default.mk
+++ properties/default.mk
@@ -1,11 +1,3 @@
-# Adb
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-# /vendor/default.prop is force-setting ro.adb.secure=1
-# Get rid of that by overriding it in /product on eng builds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.secure=0 \
-endif
-
 # Priv-app permission
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.control_privapp_permissions=log
